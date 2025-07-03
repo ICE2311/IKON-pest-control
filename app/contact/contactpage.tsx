11c6ec63b87a0b6c1f12b834e9a66dff7e742610
icys1211 <icyzoid@gmail.com>
2025-07-03 13:37:07 +0530

    some un wanted changed

--- app/contact/contactpage.tsx
+++ app/contact/contactpage.tsx
@@ -158,7 +158,7 @@ export default function ContactPage() {
                     <Mail className="h-5 w-5 text-green-600" />
                     <div>
                       <p className="font-semibold text-gray-900">Email</p>
-                      <p className="text-gray-600">info@pestguard.com</p>
+                      <p className="text-gray-600">info@ikonpest.com</p>
                     </div>
                   </div>
                 </CardContent>
