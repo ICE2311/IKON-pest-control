cfc009c068a8b4bdc46169498a2f542bb7a327a9
icys1211 <icyzoid@gmail.com>
2025-07-11 17:51:14 +0530

    seo changes

--- app/services/ticks-treatment/page.tsx
+++ app/services/ticks-treatment/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import TickTreatmentPage from './tt';
 
 export const metadata = {
-    title: 'Termite Treatment - IKON',
-    description: 'Effective termite treatment using heat and chemical methods',
+    title: 'Ticks Treatment - IKON',
+    description: 'Effective ticks treatment using heat and chemical methods',
 };
 
 const page = () => {
