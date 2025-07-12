2f6c3d4dcb1fe02dc68bd17cceca51801007eb1d
icys1211 <icyzoid@gmail.com>
2025-07-12 18:29:08 +0530

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
+import Script from "next/script";
+import FacebookPixel from "@/components/facebookPixel";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -22,8 +24,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Facebook Pixel Script */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1237238187894851');
+        fbq('track', 'PageView');
+      `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
+        <FacebookPixel />
         <Header />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1237238187894851&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <main>{children}<SpeedInsights /></main>
         <Footer />
       </body>
